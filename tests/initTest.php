1c3843dc6f0540b715ddaed80c4c6b84b3cbbaf2
SeaniRomeo <47977399+SeaniRomeo@users.noreply.github.com>
2020-05-02 22:35:51 +0200

    Update initTest.php

--- tests/initTest.php
+++ tests/initTest.php
@@ -6,7 +6,7 @@ class initTest extends PHPUnit_Framework_TestCase{
   protected $hello;
   
   public function setUp(){//this part of the code initiates the hello variable
-    $this->hello = new Software-Chasers1-0\Software-Chasers\init();
+    $this->hello = new \Rovholo\datalabel\init();
   }
   /**
    * @covers ::world
