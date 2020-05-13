2086857d2b45018a9235ad37790ce6997124934e
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-05-13 02:39:06 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,134 +1,67 @@
-function doLogin() {
-    //alert("hallo login1");
-    document.getElementById("signup-form").style.display = "none";
-    //alert("hallo login2");
-    document.getElementById("login-form").style.display = "block";
-    //document.getElementById("login").style.display = "none";
-}
-
-function doRegister() {
-    //alert("hallo register");
-    document.getElementById("signup-form").style = "display: block";
-    document.getElementById("login-form").style = "display: none";
-}
-//var res;
-function login() {
-    //alert("login");
-    
-    var res,resString;
-    var Uname = document.forms["login-form"]["username"].value;
-    //alert(Uname);
-    var pass = document.forms["login-form"]["password"].value;
-    var formData = new FormData();
-    formData.set('Uname', Uname);
-    
-    var httpReq = new XMLHttpRequest();
-    httpReq.open("POST", "php/login.php", false);
-    //httpReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    httpReq.onload = function() {
-        resString = this.responseText;
-        //alert(this.responseText);
-        res = JSON.parse(this.responseText);
-        //alert("something");
-        //alert(res);
-    };
-    httpReq.send(formData);
-    //alert(pass);
-    
-    if(resString === "[]" || pass === "" || res[0].password != pass ) {
-        alert("invalid details!!!");
-        //d = document.getElementById("demo");
-        //d.setAttribute("align", "center");
-        //d.style.color = "red";
-        //d.innerHTML = "<p>invalid username or password 1</p>";
-        //return false;
-    }
-    else {
-        alert("Login Sucesss!!!!");
-    }
-    //doLogin();
-    
-    //alert("login success");
-    //var loginCookie = document.cookie = resString;
-    //var loginCookie = document.cookie;
-    //alert(res);
-    //alert(res[0].user_role);
-    /*if(res[0].user_role === "manager") {
-        self.location = "/upload.html";
-        return false;
-    }*/
-    //self.location = "/download.html";
-    //return false;
-}
-
-function register() {
-    //alert("script activated");
-    var res;
-    var user = document.forms["signup-form"]["username"].value;
-    var mail = document.forms["signup-form"]["email"].value;
-    var pass = document.forms["signup-form"]["password"].value;
-    var params = "Uname=" + user + "&Umail=" + mail + "&passwd=" + pass;
-    alert(params);
-    var formData = new FormData();
-    formData.set('Uname', user);
-    formData.set('Umail', mail);
-    formData.set('passwd', pass);
-    var httpReq = new XMLHttpRequest();
-    httpReq.open("POST", "php/register.php", false);
-    //httpReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    httpReq.onload = function() {
-        res = JSON.parse(this.responseText);
-        //alert(this.responseText);
-    };
-    httpReq.send(formData);
-    
-    //self.location = "/index.html";
-    //return false;
-}
-
-function result(r) {
-    document.cookie = JSON.stringify(r);
-    //alert(document.cookie);
-    self.location = "/view.html";
-    return false;
-}
-
-function setuser(resp) {
-    var e = document.createElement("div");
-    e.onclick = function(){result(resp)};
-    e.style.cursor = "pointer";
-    
-    var e1 = document.createElement("div");
-    //e.setAttribute("align", "left");
-    e1.style.width = "200";
-    e1.style.padding = "50";
-    //e1.style.color = "red";
-    //e1.style.border = "thin solid #000000";
-    e1.style.display = "inline-block";
-    e1.innerHTML = resp.user_name + "_" + resp.user_id;
-    
-    var e2 = document.createElement("div");
-    //e.setAttribute("align", "left");
-    e2.style.padding = "50";
-    //e1.style.color = "blue";
-    e2.style.display = "inline-block";
-    e2.innerHTML = "rating (/5.0) = " + resp.user_rating;
-    
-    e.appendChild(e1);
-    e.appendChild(e2);
-    document.getElementById("userlist").appendChild(e);
-}
-
-function viewUsers() {
+const onload = () => {
+    loadbooks();
     var res;
     var httpReq = new XMLHttpRequest();
-    httpReq.open("POST", "php/responses.php", false);
+    httpReq.open("POST", "php/menu.php", false);
+    //httpReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     httpReq.onload = function() {
+        //res = JSON.parse(this.responseText);
+        //alert(this.responseText);
+        //document.getElementById('doc').innerHTML = this.responseText;
         res = JSON.parse(this.responseText);
-        for(var i = 0; i < res.length; i ++) {
-            setuser(res[i]);
-        }
+        //image = document.getElementById('img');
+        //alert(resarr[0].img_data);
+        //image.src = resarr[0].img_data;
+        //index = 0;
+        //document.getElementById('cover').appendChild(image);
     };
     httpReq.send("");
     
-}
+    //let books = document.getElementById('books-cover');
+    //books.innerHTML = "";
+    res.forEach( v => {
+        //alert(v.subcategory_name);
+        document.getElementById(v.category_id).innerHTML 
+            += '<li><a href="#" onclick="loadbooks(\''+v.subcategory_name+'\')">'+v.subcategory_name+'</a></li>';
+    });
+ }
+ 
+ const loadbooks = (query = "") => {
+    var resarr;
+    //alert("something");
+    let heading = document.getElementById('heading');
+    if( query === "") {
+        query = document.getElementById('search').value;
+        //alert("something1");
+    }
+    if( query === "") {
+        heading.innerHTML = "<u>Recently Uploaded</u>";
+        //alert("something2");
+    }
+    else {
+        heading.innerHTML = "<u>Search Results: </u><i>" + query+"</i>";
+        //alert("something3");
+    }
+    //alert("something");
+    var httpReq = new XMLHttpRequest();
+    httpReq.open("POST", "php/books.php", false);
+    var formData = new FormData();
+    formData.set('book', query);
+    //httpReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    httpReq.onload = function() {
+        resarr = JSON.parse(this.responseText);
+        image = document.getElementById('img');
+    };
+    httpReq.send(formData);
+    
+    let books = document.getElementById('books-cover');
+    books.innerHTML = "";
+    resarr.forEach( v => {
+        books.innerHTML += '<div class="books">'
+                            + '<img src="'+v.book_image+'" alt="IMG" width="120" height="150" />'
+                            + '<a>'+v.book_name+'</a> <br />' + '<a>R'+v.book_price+'</a>' +
+                            '</div>';
+    });
+    //alert("something");
+    return false;
+ }
--- js/upload.js
+++ js/upload.js
@@ -1,11 +1,13 @@
-function upload() {
+const upload=()=> {
     
-    alert("something1");
+    //alert("something1");
     var file = document.getElementById('fileupload');
     var bookname = document.forms["book-form"]["bookname"].value;
     var author = document.forms["book-form"]["author"].value;
     var price = document.forms["book-form"]["price"].value;
     var isbn = document.forms["book-form"]["isbn"].value;
+    var faculty = document.forms["book-form"]["faculty"].value;
+    var school = document.forms["book-form"]["school"].value;
     //alert("something");
     //alert(document.cookie);
     //alert(document.cookie[0]);
@@ -17,13 +19,15 @@ function upload() {
     formData.set('author', author);
     formData.set('price', price);
     formData.set('isbn', isbn);
+    formData.set('faculty', faculty);
+    formData.set('school', school);
     formData.set('file', file.files[0]);
     var httpReq = new XMLHttpRequest();
     httpReq.open("POST", "php/upload.php", false);
     //httpReq.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     httpReq.onload = function() {
         //alert("something2");
-        //res = this.responseText;
+        //alert(this.responseText);
         res = JSON.parse(this.responseText);
         //alert("something3");
         
@@ -34,5 +38,5 @@ function upload() {
     };
     httpReq.send(formData);
     
-    return false;
+    //return false;
 }
