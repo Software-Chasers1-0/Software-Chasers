ba2e44db6c05515bd35d92192e1e6bdd41a51ae7
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-05-23 10:50:00 +0200

    Add files via upload

--- php/books.php
+++ php/books.php
@@ -12,16 +12,23 @@ class books {
             die("connection failed: " . $link->connect_error);
         }
         $book = $_POST['book'];
-        $result = mysqli_query($link,
-            "SELECT * FROM books WHERE book_name LIKE '%$book%'
-            OR book_author LIKE '%$book%'
-            OR subcategory_id IN (
-                SELECT subcategory_id FROM SubCategories WHERE subcategory_name LIKE '%$book%'
-                OR category_id IN (
-                    SELECT category_id FROM Categories WHERE category_name LIKE '%$book%'
-                )
-            ) ORDER BY book_id DESC;"
-        );
+        $id = $_POST['id'];
+        if(is_null($id)) {
+            $result = mysqli_query($link,
+                "SELECT * FROM books WHERE book_name LIKE '%$book%'
+                OR book_author LIKE '%$book%'
+                OR subcategory_id IN (
+                    SELECT subcategory_id FROM SubCategories WHERE subcategory_name LIKE '%$book%'
+                    OR category_id IN (
+                        SELECT category_id FROM Categories WHERE category_name LIKE '%$book%'
+                    )
+                ) ORDER BY book_id DESC;"
+            );
+        }
+        else {
+            $result = mysqli_query($link,"SELECT * FROM books WHERE user_id = '$id'");
+        }
+        
     
         $link->close();
         #echo "connected successfully <br />";
