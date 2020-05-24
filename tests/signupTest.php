806883727d0785064b18590bc0a688ff273cb57a
Givengio <30724296+Givengio@users.noreply.github.com>
2020-05-24 17:57:34 +0200

    Add files via upload

---
+++ tests/signupTest.php
@@ -0,0 +1,17 @@
+<?php
+	use PHPUnit\Framework\TestCase;
+
+	/**
+	 * 
+	 */
+	class signupTest extends TestCase
+	{
+		
+		function sign()
+		{
+			$servername = "localhost";
+			$username = "rovho10";
+			$password = "Differ123*cpanel";
+		}
+	}
+?>
