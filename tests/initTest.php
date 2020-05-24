c8a1f2fc6d1cc0c38f2e7102c510674f6c76f6c5
Siya-stackit <61430304+Siya-stackit@users.noreply.github.com>
2020-05-24 14:12:02 +0200

    Update initTest.php

--- tests/initTest.php
+++ tests/initTest.php
@@ -3,10 +3,10 @@
  * @coversDefaultClass \SoftwareChasers10\SoftwareChasers\init
  */
 class initTest extends PHPUnit\Framework\TestCase{
-  protected $hello;
+  //protected $hello;
   
   public function setUp(): void {//this part of the code initiates the hello variable
-    $this->hello = new \SoftwareChasers10\SoftwareChasers\init();
+    $this->$hello = new \SoftwareChasers10\SoftwareChasers\init();
   }
   /**
    * @covers ::world
