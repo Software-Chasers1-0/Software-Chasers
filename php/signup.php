53704e04ced39a903431794ee1da0b8e11b067d1
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-05-25 16:04:31 +0200

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -18,14 +18,7 @@ class signup {
         #echo "now". $user. "thats ric";
         $result = mysqli_query( $link,"INSERT INTO users(user_name, user_email, user_password) VALUES('$user', '$mail', '$pass')" );
         $link->close();
-        //echo "connected successfully <br />";
-        //echo json_encode($result);
-        $arr = array();
-        while ($row = mysqli_fetch_assoc($result)) {
-            #echo json_encode($result);
-            array_push($arr, $row);
-        }
-        echo json_encode($arr);
+        echo $result;
     }
 }
 #echo (new signup())->sign();
