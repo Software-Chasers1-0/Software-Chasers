a35646ac563743603ea0da62568898f4bf2f138f
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-05-25 19:18:56 +0200

    Update book.php

--- php/book.php
+++ php/book.php
@@ -1,7 +1,7 @@
 <?php
 namespace SoftwareChasers10\SoftwareChasers;
 class book {
-    public function download() {
+    public function download($booksub,$bookid) {
         $servername = "localhost";
         $username = "rovhol0";
         $password = "Differ123*cpanel";
@@ -11,8 +11,8 @@ class book {
         if ($link->connect_error) {
             die("connection failed: " . $link->connect_error);
         }
-        $booksub = $_POST['book_sub'];
-        $bookid = $_POST['book_id'];
+        //$booksub = $_POST['book_sub'];
+        //$bookid = $_POST['book_id'];
         $result = mysqli_query($link,
             "SELECT SubCategories.subcategory_name,Categories.category_name,books.book_image,books.user_id FROM SubCategories,Categories,books
             WHERE SubCategories.subcategory_id = '$booksub' AND Categories.category_id = (
@@ -28,5 +28,5 @@ class book {
         return json_encode($arr);
     }
 }
-echo (new book)->download();
+//echo (new book)->download();
 ?>
