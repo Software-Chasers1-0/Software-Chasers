57a1bac3a5dac93c21931af4b94006bc71e5a09d
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-05-25 16:45:48 +0200

    Update signinTest.php

--- tests/signinTest.php
+++ tests/signinTest.php
@@ -10,14 +10,14 @@ class signinTest extends PHPUnit\Framework\TestCase{
 		$this->result = new \SoftwareChasers10\SoftwareChasers\signin();
 		$link = mysqli_connect("localhost","user","password","database1");
 		mysqli_query($link,"CREATE TABLE users (user_id int NOT NULL,user_name varchar(255),user_password varchar(255),user_email varchar(255),user_role varchar(255))");
-		mysqli_query($link,"INSERT INTO users (user_id,user_name,user_password,user_role,user_email) values('46','Pfariso','Pfariso.@1','null','mpfumbapfariso@gmail.com')");
+		mysqli_query($link,"INSERT INTO users (user_id,user_name,user_password,user_role,user_email) values('46','pfariso','Pfariso.@1','null','mpfumbapfariso@gmail.com')");
 	}
 	/**
 	 * @covers ::sign
 	 */
 	public function testsingin(){//this part of the code checks if the value returned by the world() method is equal to word
 		#$password = "Pfariso.@1";
-		$this->assertSame( 'Pfariso.@1',json_decode( $this->result->sign("Pfariso"),true )[0]["user_password"] );
+		$this->assertSame( 'Pfariso.@1',json_decode( $this->result->sign("pfariso"),true )[0]["user_password"] );
 		#$this->assertTrue( count( json_decode( $this->result->sign("") ) ) == 0 );
 	}
 }
