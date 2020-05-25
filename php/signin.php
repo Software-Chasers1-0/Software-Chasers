def929af8febfce71c3430a3b6c4965dc0f76584
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-05-25 15:40:10 +0200

    Update signin.php

--- php/signin.php
+++ php/signin.php
@@ -12,7 +12,7 @@ class signin {
             die("connection failed: " . $link->connect_error);
         }
         if(is_null($user)) {
-            $user = $_POST['Uname'];
+            #$user = $_POST['Uname'];
         }
         //echo "now". $user. "thats ric";
         $result = mysqli_query($link,"SELECT * FROM users where user_name='$user'");
