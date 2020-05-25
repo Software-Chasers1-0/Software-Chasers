e44e7b0826f111a2b37d5ed57d0f358fc7ef66f6
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-05-25 15:37:29 +0200

    Update signinTest.php

--- tests/signinTest.php
+++ tests/signinTest.php
@@ -3,7 +3,7 @@
 /**
  * @coversDefaultClass SoftwareChasers10\SoftwareChasers\signin
  */
-class signinTest extends PHPUnit_Framework_TestCase{
+class signinTest extends PHPUnit\Framework\TestCase{
 	protected $result;
 
 	public function setUp() : void {//this part of the code initiates the hello variable
