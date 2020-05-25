2f087bf7ca14b0551b21da4311e47746f8d074a7
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-05-25 16:58:45 +0200

    Update signinTest.php

--- tests/signinTest.php
+++ tests/signinTest.php
@@ -17,7 +17,7 @@ class signinTest extends PHPUnit\Framework\TestCase{
 	 */
 	public function testsingin(){//this part of the code checks if the value returned by the world() method is equal to word
 		$password = "Pfariso.@2";
-		echo "this" . $this->result->sign("pfariso");
+		echo "this";
 		echo "that" . json_decode( $this->result->sign("pfariso"),true )[0];
 		echo "now" . json_decode( $this->result->sign("pfariso"),true )[0]['user_password'];
 		$this->assertSame( json_decode( $this->result->sign("pfariso"),true )[0]['user_password'], $password );
