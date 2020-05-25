75aceaed7638cc89c8733227d069fa01aa3b087c
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-05-25 15:09:13 +0200

    Update signinTest.php

--- tests/signinTest.php
+++ tests/signinTest.php
@@ -8,7 +8,7 @@ class signinTest extends PHPUnit\Framework\TestCase{
 
 	public function setUp() : void {//this part of the code initiates the hello variable
 		$this->result = new \SoftwareChasers10\SoftwareChasers\signin();
-		$link = mysqli_connect("localhost","user","password","rovhol0_database1");
+		$link = mysqli_connect("localhost","user","password","database1");
 		mysqli_query($link,"CREATE TABLE users (user_id int NOT NULL,user_name varchar(255),user_password varchar(255),user_email varchar(255),user_role varchar(255))");
 		mysqli_query($link,"INSERT INTO users (user_id,user_name,user_password,user_email,user_role) values('46','Pfariso','Pfar','mpfumbapfariso@gmail.com','NULL')");
 	}
