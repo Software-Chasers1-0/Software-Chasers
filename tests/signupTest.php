11963638d37bb8063dc5044874f6e9be8a3e0a82
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-05-25 18:46:30 +0200

    Update signupTest.php

--- tests/signupTest.php
+++ tests/signupTest.php
@@ -8,19 +8,18 @@ class signupTest extends PHPUnit\Framework\TestCase{
   public function setUp() : void {//this part of the code initiates the hello variable
     $this->result = new \SoftwareChasers10\SoftwareChasers\signup();
     $link = mysqli_connect("localhost","user","password","database1");
-    mysqli_query($link,"CREATE TABLE users (user_id int NOT NULL,user_name varchar(255),user_password varchar(255),user_email varchar(255),user_role varchar(255))");
+    mysqli_query($link,"CREATE TABLE users (user_id int,user_name varchar(255),user_password varchar(255),user_email varchar(255),user_role varchar(255))");
 	mysqli_query($link,"INSERT INTO users (user_id,user_name,user_password,user_role,user_email) values('46','Pfariso','Pfariso.@1','null','mpfumbapfariso@gmail.com')");
   }
   /**
    * @covers ::sign
    */
   public function testsignup(){
-    #$this->assertEquals($this->result->signup("","email","pass"),'invalid input');
-    #$this->assertEquals($this->result->signup("user","","pass"),'invalid input');
-    #$this->assertEquals($this->result->signup("user","email",""),'invalid input');
-    #$this->assertEquals($this->result->signup("","",""),'invalid input');
-    #$this->assertEquals($this->result->signup("Pfariso","email","Pfariso.@1"),'invalid email');
-    #$this->assertTrue($this->result->signup("Pfariso","mpfumbapfariso@gmail.com","Pfariso.@1"));
+    $this->assertEquals($this->result->signup("","email","pass"),'invalid input');
+    $this->assertEquals($this->result->signup("user","","pass"),'invalid input');
+    $this->assertEquals($this->result->signup("user","email",""),'invalid input');
+    $this->assertEquals($this->result->signup("","",""),'invalid input');
+    $this->assertEquals($this->result->signup("Pfariso","email","Pfariso.@1"),'invalid email');
     $this->assertTrue($this->result->sign("Pfariso","mpfumbapfariso@gmail.com","Pfariso.@1"));
   }
 }
