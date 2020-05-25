79141bc7e26611bf9762cd836381907567537ecd
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-05-25 15:56:44 +0200

    Update signupTest.php

--- tests/signupTest.php
+++ tests/signupTest.php
@@ -7,7 +7,7 @@ class signupTest extends PHPUnit\Framework\TestCase{
   
   public function setUp() : void {//this part of the code initiates the hello variable
     $this->result = new \SoftwareChasers10\SoftwareChasers\signup();
-    $link = mysqli_connect("localhost","username","password","rovhol0_database1");
+    $link = mysqli_connect("localhost","user","password","rovhol0_database1");
     mysqli_query($link,"CREATE TABLE users (user_id int NOT NULL,user_name varchar(255),user_password varchar(255),user_email varchar(255),user_role varchar(255))");
 	mysqli_query($link,"INSERT INTO users (user_id,user_name,user_password,user_role,user_email) values('46','Pfariso','Pfariso.@1','null','mpfumbapfariso@gmail.com')");
   }
