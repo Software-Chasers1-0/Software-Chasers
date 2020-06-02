f6c9dcd7dcb75be61c09fade0259e65b2432f94f
Siya-stackit <61430304+Siya-stackit@users.noreply.github.com>
2020-06-02 11:26:28 +0200

    Update menuTest.php

--- tests/menuTest.php
+++ tests/menuTest.php
@@ -17,7 +17,7 @@ class menuTest extends TestCase{
    */
     public function test_if_Empty()
     {
-        $this->assertNotEmpty($this->DataHolder->download());
+        $this->assertNotEmpty($this->DataHolder);
     }
 }
 ?>
