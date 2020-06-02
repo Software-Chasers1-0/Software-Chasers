2c265fecccc8e5e1f623709c0be544cd061bedff
Siya-stackit <61430304+Siya-stackit@users.noreply.github.com>
2020-06-02 10:41:18 +0200

    Add files via upload

---
+++ tests/ChatTest.php
@@ -0,0 +1,23 @@
+<?php
+//use PHPUnit\Framework\TestCase;
+namespace PHPUnit\Framework;
+
+/**
+ * @coversDefaultClass \SoftwareChasers10\SoftwareChasers\chat
+ */
+class ChatTest extends TestCase{
+    protected $DataHolder;
+  
+    public function setUp(): void {
+    $this->DataHolder = new \SoftwareChasers10\SoftwareChasers\chat();
+   
+    }
+   /**
+   * @covers ::send_chat
+   */
+    public function test_if_Empty()
+    {
+        $this->assertNotEmpty($this->DataHolder, "It is empty");
+    }
+}
+?>
