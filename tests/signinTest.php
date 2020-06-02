b392e6cad9363666850479f10dfe470bb4ea4f04
Siya-stackit <61430304+Siya-stackit@users.noreply.github.com>
2020-06-02 09:50:39 +0200

    Add files via upload

--- tests/signinTest.php
+++ tests/signinTest.php
@@ -1,24 +1,23 @@
-
 <?php
-/**
- * @coversDefaultClass SoftwareChasers10\SoftwareChasers\signin
- */
-class signinTest extends PHPUnit\Framework\TestCase{
-	protected $result;
 
-	public function setUp() : void {//this part of the code initiates the hello variable
-		$this->result = new \SoftwareChasers10\SoftwareChasers\signin();
-		$link = mysqli_connect("localhost","user","password","database1");
-		mysqli_query($link,"CREATE TABLE users (user_id int NOT NULL,user_name varchar(255),user_password varchar(255),user_email varchar(255),user_role varchar(255))");
-		mysqli_query($link,"INSERT INTO users (user_id,user_name,user_password,user_role,user_email) values('21','Pfariso','Pfariso.@1','null','mpfumbapfariso@gmail.com')");
-	}
-	/**
-	 * @covers ::sign
-	 */
-	public function testsingin(){//this part of the code checks if the value returned by the world() method is equal to word
-		$password = "Pfariso.@1";
-		$this->assertSame( json_decode( $this->result->sign("Pfariso"),true )[0]['user_password'], $password );
-		$this->assertTrue( count( json_decode( $this->result->sign("") ) ) == 0 );
-	}
+namespace PHPUnit\Framework;
+
+/**
+ * @coversDefaultClass \SoftwareChasers10\SoftwareChasers\signin
+ */
+class signinTest extends TestCase{
+    protected $DataHolder;
+  
+    public function setUp(): void {
+    $this->DataHolder = new \SoftwareChasers10\SoftwareChasers\signin();
+   
+    }
+   /**
+   * @covers ::sign
+   */
+    public function test_if_Empty()
+    {
+        $this->assertNotEmpty($this->DataHolder, "It is empty");
+    }
 }
 ?>
