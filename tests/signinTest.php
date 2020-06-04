3d34d35ad6f86388c408e92fbcc10818a843f069
Given <1145881@students.wits.ac.za>
2020-06-04 20:25:21 +0200

    Update signinTest.php

--- tests/signinTest.php
+++ tests/signinTest.php
@@ -9,7 +9,7 @@ class signinTest extends PHPUnit\Framework\TestCase
   
     public function setUp(): void 
     {
-    $this->DataHolder = new \SoftwareChasers10\SoftwareChasers\signin();
+    $this->DataHolder = new \SoftwareChasers10\SoftwareChasers\php\signin;
     $link = mysqli_connect("localhost","user","password","database1");
     mysqli_query($link,"CREATE TABLE users (user_id int,user_name varchar(255),user_password varchar(255),user_email varchar(255),,user_role varchar(255) NULL)");
    
