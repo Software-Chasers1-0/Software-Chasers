056cfea51981553b1c4d0bf99297fc590d6cb970
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-06-07 17:27:09 +0200

    Update bookstest.php

--- tests/bookstest.php
+++ tests/bookstest.php
@@ -10,14 +10,16 @@ class bookstest extends TestCase{
   
     public function setUp(): void {
     $this->DataHolder = new \SoftwareChasers10\SoftwareChasers\books();
-   
+    $link = mysqli_connect("localhost","user","password","database1");
+    mysqli_query($link,"CREATE TABLE books (book_id int,book_name varchar(255),book_author varchar(255),subcategory_id int)");
+    mysqli_query($link,"INSERT INTO books (book_id,book_name,book_author,subcategory_id) VALUES('14','bookname','author','14')");
     }
    /**
    * @covers ::download
    */
     public function test_if_Empty()
     {
-        $this->assertNotEmpty($this->DataHolder, "It is empty");
+        $this->assertNotEmpty($this->DataHolder->download('14');
     }
 }
 ?>
