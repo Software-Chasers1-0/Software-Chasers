6836311de4e473586e1b33de4fe5e114ee32a214
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-06-07 16:49:30 +0200

    Update chatsTest.php

--- tests/chatsTest.php
+++ tests/chatsTest.php
@@ -10,14 +10,16 @@ class chatsTest extends TestCase{
   
     public function setUp(): void {
     $this->DataHolder = new \SoftwareChasers10\SoftwareChasers\chats();
-   
+    $link = mysqli_connect("localhost","user","password","database1");
+    mysqli_query($link,"CREATE TABLE Chats (seller_id int,chat varchar(255),buyer_id int)");
     }
    /**
    * @covers ::chat
    */
     public function test_if_Empty()
     {
-        $this->assertNotEmpty($this->DataHolder, "It is empty");
+        $this->assertNotEmpty($this->DataHolder->chat('46','61'));
+        $this->assertNotEmpty($this->DataHolder->chat('46'));
     }
 }
 ?>
