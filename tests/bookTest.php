e464c112599593f50f548074cef9f075a4a19349
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-06-07 17:43:43 +0200

    Update bookTest.php

--- tests/bookTest.php
+++ tests/bookTest.php
@@ -15,8 +15,8 @@ class bookTest extends TestCase{
     mysqli_query($link,"INSERT INTO Categories (category_id,category_name) values('1','Science')");
     mysqli_query($link,"CREATE TABLE SubCategories (subcategory_id int,subcategory_name varchar(255),category_id int)");
     mysqli_query($link,"INSERT INTO SubCategories (subcategory_id,subcategory_name,category_id) values('46','BSC','1')");
-    mysqli_query($link,"CREATE TABLE books (book_id int,book_name varchar(255),book_author varchar(255),subcategory_id int)");
-    mysqli_query($link,"INSERT INTO books (book_id,book_name,book_author,subcategory_id) VALUES('14','bookname','author','14')");
+    mysqli_query($link,"CREATE TABLE books (book_id int,book_name varchar(255),book_author varchar(255),subcategory_id int,user_id int, book_name varchar(255))");
+    mysqli_query($link,"INSERT INTO books (book_id,book_name,book_author,subcategory_id,user_id,book_image) VALUES('14','bookname','author','14','46','img')");
     
     }
    /**
