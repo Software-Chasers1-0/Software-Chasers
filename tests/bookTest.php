12802c36164dfaa8d0f0588304ae6e315e839534
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-06-07 17:40:27 +0200

    Update bookTest.php

--- tests/bookTest.php
+++ tests/bookTest.php
@@ -10,14 +10,21 @@ class bookTest extends TestCase{
   
     public function setUp(): void {
     $this->DataHolder = new \SoftwareChasers10\SoftwareChasers\book();
-
+    $link = mysqli_connect("localhost","user","password","database1");
+    mysqli_query($link,"CREATE TABLE Categories (category_id int,category_name varchar(255))");
+    mysqli_query($link,"INSERT INTO Categories (category_id,category_name) values('1','Science')");
+    mysqli_query($link,"CREATE TABLE SubCategories (subcategory_id int,subcategory_name varchar(255),category_id int)");
+    mysqli_query($link,"INSERT INTO SubCategories (subcategory_id,subcategory_name,category_id) values('46','BSC','1')");
+    mysqli_query($link,"CREATE TABLE books (book_id int,book_name varchar(255),book_author varchar(255),subcategory_id int)");
+    mysqli_query($link,"INSERT INTO books (book_id,book_name,book_author,subcategory_id) VALUES('14','bookname','author','14')");
+    
     }
    /**
    * @covers ::download
    */
     public function test_if_Empty()
     {
-        $this->assertNotEmpty($this->DataHolder, "It is empty");
+        $this->assertNotEmpty($this->DataHolder->download('14','14'));
     }
 }
 ?>
