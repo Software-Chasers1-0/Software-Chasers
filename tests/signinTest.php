637823d2620e3e2f8206e0a1cd89fa818dbe7259
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-06-07 15:03:38 +0200

    Update signinTest.php

--- tests/signinTest.php
+++ tests/signinTest.php
@@ -10,14 +10,15 @@ class signinTest extends TestCase{
   
     public function setUp(): void {
     $this->DataHolder = new \SoftwareChasers10\SoftwareChasers\signin();
-   
+    $link = mysqli_connect("localhost","user","password","database1");
+    mysqli_query($link,"INSERT INTO users (user_id,user_name,user_password,user_role,user_email) values('46','Pfariso','Pfariso.@1','null','mpfumbapfariso@gmail.com')");
     }
    /**
    * @covers ::sign
    */
     public function test_if_Empty()
     {
-        $this->assertNotEmpty($this->DataHolder, "It is empty");
+        $this->assertNotEmpty($this->DataHolder->sign('Pfariso'));
     }
     
     public function login()
