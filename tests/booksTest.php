22b6672b74ba5e61a87eaef40ee24bb22d52abf4
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-06-08 09:44:30 +0200

    Update booksTest.php

--- tests/booksTest.php
+++ tests/booksTest.php
@@ -11,7 +11,7 @@ class bookstest extends TestCase{
     public function setUp(): void {
     $this->DataHolder = new \SoftwareChasers10\SoftwareChasers\books();
     $link = mysqli_connect("localhost","user","password","database1");
-    mysqli_query($link,"CREATE TABLE books (book_id int,book_name varchar(255),book_author varchar(255),subcategory_id int,user_id int, book_name varchar(255))");
+    mysqli_query($link,"CREATE TABLE books (book_id int,book_name varchar(255),book_author varchar(255),subcategory_id int,user_id int, book_image varchar(255))");
     mysqli_query($link,"INSERT INTO books (book_id,book_name,book_author,subcategory_id,user_id,book_image) VALUES('14','bookname','author','14','14','img')");
 
     }
