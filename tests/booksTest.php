a9ff595838cac90cc010e90fb63def7e430448cc
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-06-08 10:09:41 +0200

    Update booksTest.php

--- tests/booksTest.php
+++ tests/booksTest.php
@@ -24,7 +24,7 @@ class bookstest extends TestCase{
    */
     public function test_if_Empty()
     {
-        #$this->assertNotEmpty($this->DataHolder->download('14'));
+        $this->assertNotEmpty($this->DataHolder->download('14',NULL));
         $this->assertNotEmpty($this->DataHolder->download('14','14'));
     }
 }
