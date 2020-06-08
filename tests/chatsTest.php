12fe2e5b321e7d254a2b5e51636341f00c7c6fd1
MpfumbaP <61417429+MpfumbaP@users.noreply.github.com>
2020-06-08 10:29:29 +0200

    Update chatsTest.php

--- tests/chatsTest.php
+++ tests/chatsTest.php
@@ -12,6 +12,7 @@ class chatsTest extends TestCase{
     $this->DataHolder = new \SoftwareChasers10\SoftwareChasers\chats();
     $link = mysqli_connect("localhost","user","password","database1");
     mysqli_query($link,"CREATE TABLE Chats (seller_id int,chat varchar(255),buyer_id int)");
+    mysqli_query($link,"CREATE TABLE users (user_id int,user_name varchar(255),user_password varchar(255),user_email varchar(255))");
     mysqli_query($link,"INSERT INTO users (user_id,user_name,user_password,user_email) values('46','Pfariso','Pfariso.@1','mpfumbapfariso@gmail.com')");
     mysqli_query($link,"INSERT INTO users (user_id,user_name,user_password,user_email) values('61','Pfariso','Pfariso.@1','mpfumbapfariso@gmail.com')");
     }
