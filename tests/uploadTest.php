688afe1e70d2b5968176fad6ca3f7824646605ef
Makgethwa <30526354+Makgethwa@users.noreply.github.com>
2020-06-10 13:07:34 +0200

    Update uploadTest.php

--- tests/uploadTest.php
+++ tests/uploadTest.php
@@ -2,9 +2,6 @@
 
 namespace PHPUnit\Framework;
 
-/**
- * @coversDefaultClass \SoftwareChasers10\SoftwareChasers\upload
- */
 class uploadTest extends TestCase{
     protected $DataHolder;
   
@@ -15,14 +12,12 @@ class uploadTest extends TestCase{
     mysqli_query($link,"INSERT INTO Categories (category_id,category_name) values('14','Science')");
     mysqli_query($link,"CREATE TABLE SubCategories (subcategory_id int,subcategory_name varchar(255),category_id int)");
     mysqli_query($link,"INSERT INTO SubCategories (subcategory_id,subcategory_name,category_id) values('14','BSC','14')");
-    mysqli_query($link,"CREATE TABLE books (book_id int,book_isbn varchar(125),book_name varchar(255),book_author varchar(255),book_price int(11),subcategory_id int,user_id int, book_image longtext)");
+    mysqli_query($link,"CREATE TABLE books (book_id int,book_isbn varchar(125),book_name varchar(255),book_author varchar(255),book_price int(11),subcategory_id, int,user_id int, book_image longtext)");
     mysqli_query($link,"INSERT INTO books (book_id,book_isbn,book_name,book_author,book_price,subcategory_id,user_id,book_image) VALUES('5','0192770950','warrior scarlet','rosematy sutcliff','225','14','2','data:;base64,')");
    
    
     }
-   /**
-   * @covers ::upload
-   */
+  
     public function test_if_Empty()
     {
         $this->assertFalse($this->DataHolder->sell('warrior scarlet','rosematy sutcliff','225','0192770950','Science','BSC','2','data:;base64,'));
